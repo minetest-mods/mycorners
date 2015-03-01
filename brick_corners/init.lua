60bc121d8063270c7f501afe8b72ea36deebac85
DonBatman <serfdon@gmail.com>
2015-03-01 13:58:49 -0800

    First Upload

---
+++ brick_corners/init.lua
@@ -0,0 +1,3 @@
+dofile(minetest.get_modpath("brick_corners").."/stone_brick.lua")
+
+dofile(minetest.get_modpath("brick_corners").."/light_stone_brick.lua")
---
+++ wood_corners/init.lua
@@ -0,0 +1,60 @@
+local wood_corners = {   --Material , Description , Image , Item
+	{ "default_cobble" , "Cobble" , "default_cobble", "default:cobble"},
+	{ "default_stone" , "Stone" , "default_stone", "default:stone"},	
+	{ "default_stone_brick" , "Stone Brick" , "default_stone_brick", "default:stonebrick"},
+	{ "default_brick" , "brick" , "default_brick", "default:brick"},
+	{ "default_wood" , "Wood" , "default_wood", "default:wood"},
+	{ "default_pinewood" , "Pine Wood" , "default_pinewood", "default:pinewood"},
+	{ "default_sandstone" , "Sandstone " , "default_sandstone", "default:sandstone"},
+	{ "default_sandstone_brick" , "Sandstone Brick" , "default_sandstone_brick", "default:sandstonebrick"},
+}
+
+for i in ipairs(wood_corners) do
+	local mat = wood_corners[i][1]
+	local desc = wood_corners[i][2]
+	local img = wood_corners[i][3]
+	local item = wood_corners[i][4]
+
+minetest.register_node("wood_corners:wood_"..mat.."_corner", {
+	description = "Wood "..desc.." Corner",
+	drawtype = "normal",
+	paramtype = "light",
+	tiles = {
+		img..".png",
+		img..".png",
+		img..".png",
+		img..".png^corners_wood1.png",
+		img..".png",
+		img..".png^corners_wood2.png",
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky = 2},
+
+})
+
+minetest.register_craft({
+	output = "wood_corners:wood_"..mat.."_corner 1",
+	recipe = {
+		{item,"wood_corners:board",""},
+		{"wood_corners:board","",""},
+		{"","",""},
+	}
+})
+
+end
+
+--boards
+minetest.register_craftitem("wood_corners:board", {
+	description = "Board",
+	inventory_image = "corners_wood1.png",
+})
+
+minetest.register_craft({
+	output = "wood_corners:board 18",
+	recipe = {
+		{"default:wood","",""},
+		{"default:wood","",""},
+		{"default:wood","",""},
+	}
+})
