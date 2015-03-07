f9fb55159695779c33b7395348404a136a61ecf9
DonBatman <serfdon@gmail.com>
2015-03-07 15:56:01 -0800

    Added grey and white wood corners

--- wood.lua
+++ wood.lua
@@ -41,7 +41,58 @@ minetest.register_craft({
 		{"","",""},
 	}
 })
+minetest.register_node("mycorners:wood_"..mat.."_grey_corner", {
+	description = "Grey Wood "..desc.." Corner",
+	drawtype = "normal",
+	paramtype = "light",
+	tiles = {
+		img..".png",
+		img..".png",
+		img..".png",
+		img..".png^corners_wood3.png",
+		img..".png",
+		img..".png^corners_wood4.png",
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky = 2},
 
+})
+
+minetest.register_craft({
+	output = "mycorners:wood_"..mat.."_grey_corner 1",
+	recipe = {
+		{item,"mycorners:board_grey",""},
+		{"mycorners:board_grey","",""},
+		{"","",""},
+	}
+})
+minetest.register_node("mycorners:wood_"..mat.."_white_corner", {
+	description = "White Wood "..desc.." Corner",
+	drawtype = "normal",
+	paramtype = "light",
+	tiles = {
+		img..".png",
+		img..".png",
+		img..".png",
+		img..".png^corners_wood6.png",
+		img..".png",
+		img..".png^corners_wood5.png",
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky = 2},
+
+})
+
+minetest.register_craft({
+	output = "mycorners:wood_"..mat.."_white_corner 1",
+	recipe = {
+		{item,"mycorners:board_white",""},
+		{"mycorners:board_white","",""},
+		{"","",""},
+	}
+})
 end
 
 --boards
@@ -56,5 +107,29 @@ minetest.register_craft({
 		{"default:wood","",""},
 		{"default:wood","",""},
 		{"default:wood","",""},
+	}
+})
+minetest.register_craftitem("mycorners:board_grey", {
+	description = "Board",
+	inventory_image = "corners_wood3.png",
+})
+minetest.register_craft({
+	output = "mycorners:board_grey 18",
+	recipe = {
+		{"mycorners:board","dye:grey",""},
+		{"","",""},
+		{"","",""},
+	}
+})
+minetest.register_craftitem("mycorners:board_white", {
+	description = "Board",
+	inventory_image = "corners_wood6.png",
+})
+minetest.register_craft({
+	output = "mycorners:board_white 18",
+	recipe = {
+		{"mycorners:board","dye:white",""},
+		{"","",""},
+		{"","",""},
 	}
 })
